3f6d119df43ff1e56419a6395fbc0aafacaabd0b
Muhammad Jafar Shidik <62690663+mjafarshidik@users.noreply.github.com>
2024-07-18 22:24:08 +0700

    Initial commit

---
+++ app/src/main/java/com/penatabahasa/workshop1/ui/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.penatabahasa.workshop1.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.penatabahasa.workshop1.R
+
+class MainActivity : AppCompatActivity() {
+    var firstName : string = ""
+
+    private fun playGame() {
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
